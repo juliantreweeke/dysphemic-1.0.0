026f3df9f336e2cac37da1973fa0c0fd9d2e6897
“juliantreweeke” <juliantreweeke@gmail.com>
2019-04-26 01:58:47 +1000

    working on mobile header wip

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,10 +2,45 @@ import React from 'react';
 import { Link } from 'gatsby';
 import './Header.scss';
 import Logo from '../../images/logo-yellow.png';
+import LogoInverted from '../../images/logo-yellow-inverted.png';
 
 interface HeaderProps {
   siteTitle?: string;
 }
+
+const navigation = (
+  <ul>
+    <li>
+      <Link to="/" className="header__image-mobile-link">
+        <img
+          className="header__image-mobile-open"
+          src={LogoInverted}
+          alt={'dysphemic'}
+        />
+      </Link>
+    </li>
+    <li>
+      <Link activeClassName="active" to="/music/">
+        MUSIC
+      </Link>
+    </li>
+    <li>
+      <Link activeClassName="active" to="/about/">
+        ABOUT
+      </Link>
+    </li>
+    <li>
+      <Link activeClassName="active" to="/shop/">
+        SHOP
+      </Link>
+    </li>
+    <li>
+      <Link activeClassName="active" to="/contact/">
+        CONTACT
+      </Link>
+    </li>
+  </ul>
+);
 
 class Header extends React.PureComponent<HeaderProps, {}> {
   public render() {
@@ -16,30 +51,8 @@ class Header extends React.PureComponent<HeaderProps, {}> {
           <Link to="/" className="header__link">
             <img className="header__image" src={Logo} alt={siteTitle} />
           </Link>
-          <nav className="header__nav">
-            <ul>
-              <li>
-                <Link activeClassName="active" to="/music/">
-                  MUSIC
-                </Link>
-              </li>
-              <li>
-                <Link activeClassName="active" to="/about/">
-                  ABOUT
-                </Link>
-              </li>
-              <li>
-                <Link activeClassName="active" to="/shop/">
-                  SHOP
-                </Link>
-              </li>
-              <li>
-                <Link activeClassName="active" to="/contact/">
-                  CONTACT
-                </Link>
-              </li>
-            </ul>
-          </nav>
+          <nav className="header__nav">{navigation}</nav>
+          <nav className="header__nav-mobile-open">{navigation}</nav>
         </div>
       </header>
     );
