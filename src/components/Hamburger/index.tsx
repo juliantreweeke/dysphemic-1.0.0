3ebd0ed47afb9d9ecacd9fd661954c6f6860c6b7
“juliantreweeke” <juliantreweeke@gmail.com>
2019-04-26 18:49:07 +1000

    working on responsive header

---
+++ src/components/Hamburger/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Hamburger.scss';
+
+interface HamburgerProps {
+  open: boolean;
+  toggleOpen: any;
+}
+
+const hamburger: React.SFC<HamburgerProps> = ({ open, toggleOpen }) => {
+  return (
+    <div onClick={toggleOpen} className={`hamburger ${open ? 'open' : ''}`}>
+      <span />
+      <span />
+      <span />
+      <span />
+    </div>
+  );
+};
+
+export default hamburger;
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,6 +3,7 @@ import { Link } from 'gatsby';
 import './Header.scss';
 import Logo from '../../images/logo-yellow.png';
 import LogoInverted from '../../images/logo-yellow-inverted.png';
+import Hamburger from '../Hamburger';
 
 interface HeaderProps {
   siteTitle?: string;
@@ -10,15 +11,6 @@ interface HeaderProps {
 
 const navigation = (
   <ul>
-    <li>
-      <Link to="/" className="header__image-mobile-link">
-        <img
-          className="header__image-mobile-open"
-          src={LogoInverted}
-          alt={'dysphemic'}
-        />
-      </Link>
-    </li>
     <li>
       <Link activeClassName="active" to="/music/">
         MUSIC
@@ -42,17 +34,37 @@ const navigation = (
   </ul>
 );
 
-class Header extends React.PureComponent<HeaderProps, {}> {
+class Header extends React.PureComponent<HeaderProps, { open: boolean }> {
+  state = {
+    open: false,
+  };
+
+  toggleOpen = () => {
+    this.setState({ open: !this.state.open });
+    console.log(this.state.open);
+  }
+
   public render() {
     const { siteTitle } = this.props;
+    const { open } = this.state;
     return (
       <header className="header">
         <div className="header__content">
           <Link to="/" className="header__link">
-            <img className="header__image" src={Logo} alt={siteTitle} />
+            <img
+              className="header__image"
+              src={open ? LogoInverted : Logo}
+              alt={siteTitle}
+            />
           </Link>
           <nav className="header__nav">{navigation}</nav>
-          <nav className="header__nav-mobile-open">{navigation}</nav>
+          <Hamburger toggleOpen={this.toggleOpen} open={open} />
+
+          {this.state.open && (
+            <nav onClick={this.toggleOpen} className="header__nav-mobile-open">
+              {navigation}
+            </nav>
+          )}
         </div>
       </header>
     );
